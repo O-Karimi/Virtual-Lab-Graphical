4fbbaaf2220771b91bd8bbb130ef5b39b120e6ba
O-Karimi <omidkarimi54@gmail.com>
2025-11-24 11:00:26 +0100

    Initial GUI Implemented
    The initial GUI Implemented

--- src/main/java/JavaFXInterface/HelloApplication.java
+++ src/main/java/JavaFXInterface/HelloApplication.java
@@ -8,12 +8,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("PhysicsLabGUI.fxml"));
+        Scene mainScene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(mainScene);
         stage.show();
     }
 }
--- src/main/java/JavaFXInterface/HelloController.java
+++ src/main/java/JavaFXInterface/HelloController.java
@@ -1,11 +1,13 @@
 package JavaFXInterface;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.Accordion;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
 
-    @FXML
-    private Button  particleButton;
+
 }
